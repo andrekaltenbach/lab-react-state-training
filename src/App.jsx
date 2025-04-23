cab71107d0eef10be8e3930703b0160084d4a1c9
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-04-23 12:55:24 +0200

    solve iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import "./App.css";
+import './App.css';
+import LikeButton from './components/Likebutton';
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <LikeButton /> <LikeButton />
     </div>
   );
 }
